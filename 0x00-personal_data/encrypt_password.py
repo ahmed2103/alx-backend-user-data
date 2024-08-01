ddf446c9661f4d9ade3bebd23087dab5d4545e2d
Hasona <bekoahmed20@gmail.com>
2024-08-01 17:52:27 +0300

    fix issues

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,15 +2,14 @@
 """Module to encrypt passwords"""
 
 
-from bcrypt import hashpw, gensalt, checkpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Function to encrypt passwords"""
-    salt = gensalt()
-    return hashpw(password.encode('utf-8'), salt)
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validate password and returns True if password is valid"""
-    return checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
