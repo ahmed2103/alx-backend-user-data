7a017d8d3b37cbb7596b7ee95ca0afdb4bd8ccb8
Hasona <bekoahmed20@gmail.com>
2024-08-06 15:43:38 +0300

    nnnn

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Authenication module for flask api"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Represents authenication to api"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns true if path is authorized not in excluded_paths"""
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/'
+                          ]
+        if not path or not excluded_paths:
+            return True
+        path = path.rstrip('/') + '/'
+        for ex_path in excluded_paths:
+            ex_path = ex_path.rstrip('/') + '/'
+            if path.startswith(ex_path[0:-1]) or path == ex_path:
+                return False
+        return True
+
+    def authorization_header(self, request: request = None) -> str:
+        """returns authorization header value"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """User Authorization"""
+
+        return None
