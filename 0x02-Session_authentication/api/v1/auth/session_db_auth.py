4b4cba365a0a1e55d595e62937df3df8b7023339
Hasona <bekoahmed20@gmail.com>
2024-08-08 23:39:52 +0300

    fix

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,15 +4,17 @@
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 from datetime import datetime, timedelta
+
+
 class SessionDBAuth(SessionExpAuth):
     """class for session authenication with storage safe capablity"""
-    def create_session(self, user_id=None):
+    def create_session(self, user_id=None) -> UserSession:
         """Creates a new session"""
         sess_id = super().create_session(user_id)
         UserSession(session_id=sess_id, user_id=user_id).save()
         return sess_id
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) -> str:
         """Creates a new session"""
         try:
             user_session = UserSession.search({'session_id': session_id})[0]
@@ -24,7 +26,7 @@ class SessionDBAuth(SessionExpAuth):
             return None
         return user_session.user_id
 
-    def destroy_session(self, request=None) -> str:
+    def destroy_session(self, request=None) -> bool:
         """destroys a session"""
         session_id = self.session_cookie(request)
         try:
@@ -32,9 +34,3 @@ class SessionDBAuth(SessionExpAuth):
         except Exception:
             return False
         return True
-
-
-
-
-
-
