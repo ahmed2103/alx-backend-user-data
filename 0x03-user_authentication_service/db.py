6ada2bd91df49c134632a5c46433fbe21d2169ff
Hasona <bekoahmed20@gmail.com>
2024-08-15 15:09:35 +0300

    hell

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
-from typing import Dict
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,11 +12,11 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -26,7 +25,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -34,37 +33,48 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Create a new user"""
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
-        self._session.add(user)
-        self._session.commit()
-        return user
-
-    def find_user_by(self, **attrs: Dict) -> User:
-        """Find a user by attributes"""
-        for attr in attrs:
-            if not hasattr(User, attr):
-                raise InvalidRequestError()
-        user = self._session.query(User).filter_by(**attrs).first()
-        if user is None:
-            raise NoResultFound()
-        return user
-
-    def update_user(self, user_id: int, **attrs: Dict) -> None:
-        """Updates user attributes"""
-        user = self.find_user_by(id=user_id)
-        if user is None:
-            raise NoResultFound()
-            return None
+        """Adds a new user to the database.
+        """
         try:
-            for key, value in attrs.items():
-                if hasattr(user, key):
-                    setattr(user, key, value)
-                else:
-                    raise ValueError
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            raise
+            new_user = None
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
